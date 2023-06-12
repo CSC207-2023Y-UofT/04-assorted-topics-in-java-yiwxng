f2ff7e85f587f75ea6f25ab85f1a40a55ebc2759
yiwxng <122708609+yiwxng@users.noreply.github.com>
2023-06-11 22:05:42 -0400

    Completed

--- src/main/java/DrivableMap.java
+++ src/main/java/DrivableMap.java
@@ -27,7 +27,13 @@ class DrivableMap {
      *       in drivable_map, then add the pair to drivable_map.
      *       Return true if the Drivable was added to drivable_map.
      */
-
+    public boolean addDrivable(String id, Drivable drivable) {
+        if (!drivable_map.containsKey(id)) {
+            drivable_map.put(id, drivable);
+            return true;
+        }
+        return false;
+    }
 
 
 
@@ -37,7 +43,14 @@ class DrivableMap {
      * You may want to use drivable_map.keys() or drivable_map.values() to
      * iterate through drivable_map.
      */
-
+    public boolean hasFasterThan(int speed) {
+        for (Drivable drivable : drivable_map.values()) {
+            if (drivable.getMaxSpeed() >= speed) {
+                return true;
+            }
+        }
+        return false;
+    }
 
 
 
@@ -46,7 +59,15 @@ class DrivableMap {
      *       returns a List containing all of the Tradable items in
      *       drivable_map.
      */
-
+    public List<Tradable> getTradable() {
+        List<Tradable> tradableList = new ArrayList<>();
+        for (Drivable drivable : drivable_map.values()) {
+            if (drivable instanceof Tradable) {
+                tradableList.add((Tradable) drivable);
+            }
+        }
+        return tradableList;
+    }
 
 
     
--- src/main/java/DrivableTrader.java
+++ src/main/java/DrivableTrader.java
@@ -11,3 +11,24 @@
  */
 
 import java.util.List;
+public class DrivableTrader extends Trader<Drivable> {
+
+    public DrivableTrader(List<Drivable> inventory, List<Drivable> wishlist, int money) {
+        super(inventory, wishlist, money);
+    }
+
+    public DrivableTrader(int money) {
+        super(money);
+    }
+
+    @Override
+    public int getSellingPrice(Drivable item) {
+        if (item instanceof Tradable) {
+            int basePrice = ((Tradable) item).getPrice();
+            int maxSpeed = item.getMaxSpeed();
+            return basePrice + maxSpeed;
+        } else {
+            return Tradable.MISSING_PRICE;
+        }
+    }
+}
